22d334a27ca17842105f6dfc1ad9029a2aee717d
Shadmer <Shadmer89@gmail.com>
2024-05-06 00:06:54 +0700

    Доработка бота (#1)
    Co-authored-by: dchesnokov <dchesnokov@rtk-element.ru>

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { SquareType } from './types';
+import classes from '../styles/square.module.css';
+import { Box, Button } from '@mantine/core';
 
 interface SquareProps {
     value: SquareType;
     onSquareClick: () => void;
     isOldest: boolean;
+    gradient: {
+        from: string;
+        to: string;
+        deg: number;
+    };
+    disabled: boolean;
 }
 
-export const Square = ({ value, onSquareClick, isOldest }: SquareProps) => {
+export const Square = ({
+    value,
+    onSquareClick,
+    isOldest,
+    gradient,
+    disabled,
+}: SquareProps) => {
     return (
-        <button
-            className={'square'}
-            onClick={onSquareClick}
-            style={{ opacity: isOldest ? '.3' : '1' }}
-        >
-            {value}
-        </button>
+        <Box>
+            <Button
+                onClick={onSquareClick}
+                className={`${classes.square} ${isOldest && classes.oldest}`}
+                variant="gradient"
+                gradient={gradient}
+                disabled={disabled}
+            >
+                {value}
+            </Button>
+        </Box>
     );
 };
